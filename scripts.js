ec74419023c3bea7539e01acb507c16c3b9f19cb
madrackwp <gohweipin98@gmail.com>
2022-05-30 21:21:37 +0800

    Created the logic to add books to the library and render the cards

--- scripts.js
+++ scripts.js
@@ -7,12 +7,56 @@ function Book(bookName, bookAuthor, bookPages, bookRead) {
   this.bookRead = bookRead;
 }
 
+function renderBooks(booksArray) {
+  const bookLibrary = document.querySelector(".bookLibrary");
+  while (bookLibrary.firstChild) {
+    bookLibrary.removeChild(bookLibrary.firstChild);
+  }
+
+  booksArray.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("bookCard");
+
+    const bookTitle = document.createElement("h2");
+    bookTitle.classList.add("bookName");
+    bookTitle.innerHTML = book.bookName;
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.classList.add("bookAuthor");
+    bookAuthor.innerText = book.bookAuthor;
+
+    const pageCount = document.createElement("p");
+    pageCount.classList.add("pageCount");
+    pageCount.innerText = book.bookPages;
+
+    const readButton = document.createElement("button");
+    readButton.classList.add("bookReadToggle");
+    if (book.bookRead) {
+      readButton.classList.add("read");
+      readButton.innerHTML = "Read";
+    } else {
+      readButton.classList.add("unread");
+      readButton.innerHTML = "Unread";
+    }
+
+    const removeEntryButton = document.createElement("button");
+    removeEntryButton.classList.add("removeButton");
+    removeEntryButton.innerText = "Remove";
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(pageCount);
+    bookCard.appendChild(readButton);
+    bookCard.appendChild(removeEntryButton);
+    bookLibrary.appendChild(bookCard);
+  });
+}
+
 const addBookFormToggle = document.querySelector(".add-book-form-toggle");
 const addBookForm = document.querySelector(".add-book-form");
-// addBookFormToggle.addEventListener("click", toggleForm);
+const addBookFields = document.querySelectorAll(".addBookInput");
 
 function openForm() {
-  // console.log("Open form");
   addBookForm.style.display = "flex";
   addBookFormToggle.style.display = "none";
 }
@@ -22,4 +66,23 @@ function closeForm() {
   addBookFormToggle.style.display = "block";
 }
 
-function addBook() {}
+function clearForm() {
+  addBookFields[0].value = "";
+  addBookFields[1].value = "";
+  addBookFields[2].value = "";
+  addBookFields[3].children[0].checked = false;
+}
+
+function addBook(event) {
+  event.preventDefault();
+  const bookName = addBookFields[0].value;
+  const autherName = addBookFields[1].value;
+  const noPages = addBookFields[2].value;
+  const bookRead = addBookFields[3].children[0].checked;
+
+  const newBook = new Book(bookName, autherName, noPages, bookRead);
+  myBooks.push(newBook);
+  renderBooks(myBooks);
+  closeForm();
+  clearForm();
+}
